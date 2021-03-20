87c686f42902513a569d424b8a038e5319381423
jpfiorilla <jpfiorilla@gmail.com>
2021-03-20 08:39:01 -0400

    bio

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Third } from '../../components';
+import { Link, Spacer, Third } from '../../components';
 import { Img } from '../../components/Icons';
 import './About.scss';
 
@@ -15,11 +15,13 @@ He lives in Brooklyn, New York.`;
 // brand yrself
 
 const testBio = `
-John Fio is a composer (ASCAP), multi-instrumentalist, producer, arranger, and audio engineer. When he was 11, he bootleg-recorded the entire Zelda: Majora's Mask soundtrack from a CRT TV onto a cassette player. This early love of game music later drove him to study music at Rutgers in New Jersey.
+<strong>John Fio</strong> is a composer (ASCAP), multi-instrumentalist, producer, arranger, and audio engineer.
 
-Since then, John has had the opportunity of contributing to a wide range of videogames, shortfilms, and games-culture programming. 
+When John was 11, he bootleg-recorded the entire <i>Zelda: Majora's Mask</i> soundtrack from the speakers of his TV onto a cassette player. This early love of game music later drove him to study music at Rutgers University in New Jersey.
 
-In 2019, John got 13th at EVO in Super Smash Bros. Melee. He lives in Brooklyn, New York.`.trim();
+Since then, John has had the opportunity of contributing original music to a <a href="/credits">wide range of videogames, shortfilms, and games-culture programming</a>. He also acts as a music curator for indie game venue <a href="https://www.wonderville.nyc/">Wonderville</a> in Bushwick, NYC.
+
+In 2019, John placed 13th at EVO in <i>Super Smash Bros. Melee</i>. He lives in Brooklyn, New York.`.trim();
 
 const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel ante mi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc non ipsum semper, dapibus nisl eget, eleifend quam. Nunc pharetra risus felis, eget tempus magna dictum eget. Sed vitae purus libero. Quisque non ex justo. Maecenas ac lectus at nunc fermentum imperdiet auctor id dolor. Proin molestie risus eu felis placerat, vel tempus erat sodales. Integer gravida, elit at rutrum laoreet, est tellus euismod quam, id efficitur libero neque vel ligula.
 
@@ -29,14 +31,24 @@ Etiam at orci eleifend, pharetra dui quis, aliquet eros. Nulla auctor lorem puru
 
 const About = () => (
   <div className="About">
-    <Third>
-      <div className="About--Inner">
-        <div className="About--PortraitWrapper">
+    {/* <Third> */}
+    <div className="About--Inner">
+      <div className="About--PortraitWrapper">
+        <div className="About--PortraitContent">
           <Img className="About--Portrait" src="photos/portrait-emi.jpg" />
+          <Spacer />
+          <div>
+            photo © <Link href="https://www.uglymachine.net/">Emi Spicer</Link>
+          </div>
         </div>
-        <div className="About--Copy">{bio}</div>
       </div>
-    </Third>
+      {/* <div className="About--Copy">{testBio}</div> */}
+      <div
+        className="About--Copy"
+        dangerouslySetInnerHTML={{ __html: testBio }}
+      />
+    </div>
+    {/* </Third> */}
   </div>
 );
 
