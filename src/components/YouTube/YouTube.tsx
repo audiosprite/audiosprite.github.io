9591cfe60df0276d22a555f4e764cbcd1150aa25
jpfiorilla <jpfiorilla@gmail.com>
2021-06-25 17:29:22 -0400

    fix piecewise

--- src/components/YouTube/YouTube.tsx
+++ src/components/YouTube/YouTube.tsx
@@ -6,13 +6,12 @@ type YouTubeProps = { id: string };
 export const YouTube = ({ id }: YouTubeProps) => (
   <div className="YouTube">
     <iframe
+      allowFullScreen
       width="853"
       height="480"
-      //   width="100%"
       src={`https://www.youtube.com/embed/${id}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
       title="Embedded youtube"
     />
   </div>
