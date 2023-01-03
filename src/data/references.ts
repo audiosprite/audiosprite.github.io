9cedd50d57a79c99d7b5e0135fef4ce0b3549c29
John Fiorilla <jpfiorilla@gmail.com>
2023-01-03 15:17:54 -0500

    update css-loader to 6.7.3, yarn build

---
+++ src/data/references.ts
@@ -0,0 +1,6 @@
+const references = ['https://jacob-lincke.com/'];
+
+const packages = [
+  'https://www.google.com/search?q=jsmediatags&oq=jsmediatags&aqs=chrome..69i57j0i512j0i30l5j0i5i30l2j0i10i30.1664j0j1&sourceid=chrome&ie=UTF-8',
+  'https://codesandbox.io/s/w9c0x?file=/src/components/Player/useData.js',
+];
