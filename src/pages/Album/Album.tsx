94197a9b94b98c0cea900b9e7f15548055c74299
John Fiorilla <jpfiorilla@gmail.com>
2025-12-11 12:36:22 -0500

    bandcamp avialble now

--- src/pages/Album/Album.tsx
+++ src/pages/Album/Album.tsx
@@ -2,18 +2,34 @@ import * as React from 'react';
 import { Heading } from '../../components';
 import './Album.scss';
 
+const BandcampIframe = () => (
+  <iframe
+    style={{
+      border: '0',
+      borderRadius: '16px',
+      boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.1)',
+      width: '500px',
+      height: '620px',
+      marginBottom: '75px',
+    }}
+    src="https://bandcamp.com/EmbeddedPlayer/album=2108143031/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/"
+    seamless
+    title="Skate Story: Vol. 2"
+  >
+    <a href="https://audiosprite.bandcamp.com/album/skate-story-vol-2">
+      Skate Story: Vol. 2 by John Fio
+    </a>
+  </iframe>
+);
+
 export const Album = () => {
   return (
     <div className="Album">
-      <a
-        href="https://audiosprite.bandcamp.com/album/skate-story-vol-2"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src="/build/img/albums/skate-story-vol-2.jpg" alt="Album" />
-      </a>
-      <Heading as="h2">Skate Story: Vol. 2</Heading>
-      <p>Coming soon</p>
+      <div className="Album--Header">
+        <Heading as="h2">Skate Story: Vol. 2</Heading>
+        <p>Available now</p>
+      </div>
+      <BandcampIframe />
     </div>
   );
 };
